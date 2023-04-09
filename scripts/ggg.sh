2973cc3b8e10c4d080ba99951b548d13885d9e0d
Susovan Patra <116751498+susovanpatra00@users.noreply.github.com>
2023-04-09 11:25:56 +0530

    Update ggg.sh

--- scripts/ggg.sh
+++ scripts/ggg.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
-# for file in *.sol
-# do
-#   python3 main.py 1 "$file" majority_unique >> output.csv
-# done
-
+# It runs the main.py for all the solidity files and generates the output in a csv file named output.csv
 #!/bin/bash
 
 for file in *.sol
